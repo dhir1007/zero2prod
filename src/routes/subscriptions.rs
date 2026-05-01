5c30164fbc728a7b97b9c1a7853e91d5ffd3f9d5
Dhir <dhirkatre@gmail.com>
2026-04-30 22:51:36 -0700

    Integrate `tracing` for enhanced logging in the subscription route, replacing previous `log` calls with structured logging. This allows for better tracking of subscription attempts and outcomes, including unique request IDs for improved traceability.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,6 @@
-use actix_web::{web, HttpResponse};
-use sqlx::PgPool;
+use actix_web::{HttpResponse, web};
 use chrono::Utc;
+use sqlx::PgPool;
 use uuid::Uuid;
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -9,11 +9,29 @@ pub struct FormData {
 }
 
 // we always return 200 OK for now
-pub async fn subscribe(
-    _form: web::Form<FormData>,
-    _db_pool: web::Data<PgPool>
-) -> HttpResponse {
-    log::info!("New subscription attempted: {:?}", _form.0);
+pub async fn subscribe(_form: web::Form<FormData>, _db_pool: web::Data<PgPool>) -> HttpResponse {
+
+    
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber..",
+        %request_id,
+        subscriber_email = %_form.email,
+        subscriber_name = %_form.name,
+    );
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
+
+    tracing::info!(
+        "{request_id}: New subscription attempted: {email}, {name}",
+        request_id = request_id,
+        email = _form.email,
+        name = _form.name
+    );
+    tracing::info!("{request_id}: request received");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,15 +41,20 @@ pub async fn subscribe(
         _form.email,
         _form.name,
         Utc::now()
-    ).execute(_db_pool.get_ref())
+    )
+    .execute(_db_pool.as_ref())
     .await
     {
         Ok(_) => {
-            log::info!("New subscription successful");
+            tracing::info!("{request_id}: New subscription successful");
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {}", e);
+            tracing::error!(
+                "{request_id}:Failed to execute query: {error}",
+                request_id = request_id,
+                error = e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
